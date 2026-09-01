fe09bd5fdab5d273be226fbdc5a88da729510208
kytvp1 <kytvp1@gmail.com>
2026-09-01 22:23:35 +0200

    Add files via upload

--- assets/partners.js
+++ assets/partners.js
@@ -16,8 +16,8 @@
 */
 
 const AV_PARTNERS = [
-  { image: null, link: null, label: 'Miejsce na logo' },
-  { image: null, link: null, label: 'Miejsce na logo' },
+  { image: 'assets/partners/partner-1.png', link: null, label: 'Miejsce na logo' },
+  { image: 'assets/partners/partner-2.png', link: null, label: 'Miejsce na logo' },
   { image: null, link: null, label: 'Miejsce na logo' },
   { image: null, link: null, label: 'Miejsce na logo' },
 ];
